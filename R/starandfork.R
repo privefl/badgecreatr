424fb6b1eea39178df5ae2b78710bf31735a1abf
Florian <florian.prive.21@gmail.com>
2016-08-17 23:27:54 +0200

    del to keep up

--- R/starandfork.R
+++
@@ -1,8 +0,0 @@
-user <- "RMHogervorst"
-repo <- "badgecreatr"
-
-github <- readLines(sprintf("https://github.com/%s/%s", user, repo))
-
-network <- sprintf("<a href=\"/%s/%s/network\" class=\"social-count\">", user, repo)
-4
-</a>
